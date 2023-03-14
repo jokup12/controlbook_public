572b5cebbc6d2a28b9b0d583fddd2bd05b27eb30
jokup12 <jordkpenfold@yahoo.com>
2023-03-14 14:50:33 -0600

    Updated Template

--- _D_mass/python/massParam.py
+++ _D_mass/python/massParam.py
@@ -2,28 +2,29 @@
 import numpy as np
 
 # Physical parameters of the arm known to the controller
-m =   # mass kg
-k =   # spring constant Kg/s^2
-b =   # damping coefficient Kg/s
+m = 5.0  # mass kg
+k = 3.0  # spring constant Kg/s^2
+b = .5  # damping coefficient Kg/s
+g = 9.81
 
 # parameters for animation
 length = 5.0
 width = 1.0
 
 # Initial Conditions
-z0 =   # initial position of mass, m
-zdot0 =   # initial velocity of mass m/s
+z0 = 0.0  # initial position of mass, m
+zdot0 = 0.0  # initial velocity of mass m/s
 
 # Simulation Parameters
-t_start =  # Start time of simulation
-t_end =   # End time of simulation
-Ts =   # sample time for simulation
-t_plot =  # the plotting and animation is updated at this rate
+t_start = 0 # Start time of simulation
+t_end = 120  # End time of simulation
+Ts = .01  # sample time for simulation
+t_plot = .1 # the plotting and animation is updated at this rate
 
 # dirty derivative parameters
-# sigma =  # cutoff freq for dirty derivative
+sigma = .05 # cutoff freq for dirty derivative
 # beta =   # dirty derivative gain
 
 # saturation limits
-# F_max =   # Max force, N
+F_max = 1000000  # Max force, N
 
