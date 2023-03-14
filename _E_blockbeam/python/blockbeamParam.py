a54a21068252978f5e6633d9d9d90cdeae4d8d52
jokup12 <jordkpenfold@yahoo.com>
2023-03-14 16:52:31 -0600

    Updated Parameters

--- _E_blockbeam/python/blockbeamParam.py
+++ _E_blockbeam/python/blockbeamParam.py
@@ -2,35 +2,35 @@
 import numpy as np
 # import control as cnt
 
-# Physical parameters of the  ballbeam known to the controller
-m1 =   # Mass of the block, kg
-m2 =   # mass of beam, kg
-length =   # length of beam, m
-g =   # gravity at sea level, m/s^2
+# Physical parameters of the  blockbeam known to the controller
+m1 = .35  # Mass of the block, kg
+m2 = 2.0  # mass of beam, kg
+length = .5  # length of beam, m
+g = 9.8  # gravity at sea level, m/s^2
 
 # parameters for animation
 width = 0.05  # width of block
 height = width*0.25  # height of block
 
 # Initial Conditions
-z0 =  # initial block position,m
-theta0 = *np.pi/180  # initial beam angle,rads
-zdot0 =   # initial speed of block along beam, m/s
-thetadot0 =  # initial angular speed of the beam,rads/s
+z0 = .25 # initial block position,m
+theta0 = 0*np.pi/180  # initial beam angle,rads
+zdot0 = 0.0  # initial speed of block along beam, m/s
+thetadot0 = 0.0 # initial angular speed of the beam,rads/s
 
 # Simulation Parameters
-t_start =   # Start time of simulation
-t_end =   # End time of simulation
-Ts =   # sample time for simulation
-t_plot =   # the plotting and animation is updated at this rate
+t_start = 0  # Start time of simulation
+t_end = 300  # End time of simulation
+Ts = .01  # sample time for simulation
+t_plot = .1  # the plotting and animation is updated at this rate
 
 # saturation limits
-Fmax =   # Max Force, N
+Fmax = 150000.0  # Max Force, N
 
 # dirty derivative parameters
-# sigma =   # cutoff freq for dirty derivative
+sigma = .05  # cutoff freq for dirty derivative
 # beta =  # dirty derivative gain
 
 # equilibrium force when block is in center of beam
-# ze =
-# Fe =
+ze = length/2
+Fe = (m1*g*ze)/length + m2*g/2
--- _hummingbird_sim/lab2_hummingbirdSim.py
+++ _hummingbird_sim/lab2_hummingbirdSim.py
@@ -6,9 +6,9 @@ from hummingbirdAnimation import HummingbirdAnimation
 from dataPlotter import DataPlotter
 
 # instantiate reference input classes
-phi_ref = SignalGenerator(amplitude=1.5, frequency=0.05)
-theta_ref = SignalGenerator(amplitude=0.5, frequency=0.05)
-psi_ref = SignalGenerator(amplitude=0.5, frequency=.05)
+phi_ref = SignalGenerator(amplitude=1.5, frequency=0.1)
+theta_ref = SignalGenerator(amplitude=1.5, frequency=0.1)
+psi_ref = SignalGenerator(amplitude=1.5, frequency=0.1)
 
 # instantiate the simulation plots and animation
 dataPlot = DataPlotter()
@@ -17,9 +17,9 @@ animation = HummingbirdAnimation()
 t = P.t_start  # time starts at t_start
 while t < P.t_end:  # main simulation loop
     # set variables
-    phi = phi_ref.sin(t)
-    theta = 0#theta_ref.sin(t)
-    psi = 0#psi_ref.sin(t)
+    phi = phi_ref.sin(t)            #roll, positive spin points forward
+    theta = theta_ref.sin(t)      #pitch, positive spin points starboard
+    psi = psi_ref.sin(t)          #yaw, positive spin points down
     # update animation
     state = np.array([[phi], [theta], [psi], [0.0], [0.0], [0.0]])
     ref = np.array([[0], [0], [0]])
